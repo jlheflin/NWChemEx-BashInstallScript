68146f2f01dda2b114ae21e92bc04853943639df
Jacob Heflin <btheflin@gmail.com>
2025-06-16 13:52:09 -0500

    added libint2 build

--- install_nwchemex.sh
+++ install_nwchemex.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-
 REPO_URL="https://github.com/NWChemEx/NWChemEx.git"
-INSTALL_DIR=`pwd`/install
-LIBINT2_DIR=`nix eval --raw nixpkgs#libint`
+NWCHEMEX_DIR=`pwd`/NWChemEx
+INSTALL_DIR=$NWCHEMEX_DIR/install
 
 echo "Cloning NWChemEx Repository"
 if [[ -d "NWChemEx" ]]; then
@@ -26,7 +25,7 @@ if [[ ! -d ".venv" ]]; then
   source ./.venv/bin/activate
 else
   source ./.venv/bin/activate
-  pip install ase networkx qcengine qcelemental
+  pip install ase networkx qcengine qcelemental setuptools
 fi
   
 
@@ -35,10 +34,23 @@ if [[ -d "build" ]]; then
   rm -rf build
 fi
 
+VERSION=2.9.0
+wget https://github.com/evaleev/libint/releases/download/v${VERSION}/libint-${VERSION}.tgz
+tar -zxf libint-${VERSION}.tgz
+
+LIBINT2_DIR=`pwd`/libint-${VERSION}
+LIBINT2_INSTALL=`pwd`/libint-${VERSION}/install
+cd libint-${VERSION}
+cmake -B build -S . -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE -DCMAKE_INSTALL_PREFIX=$LIBINT2_DIR/install
+cmake --build build --target install --parallel
+
+cd $NWCHEMEX_DIR
 CMAKE_FLAGS="-DNWX_MODULE_DIRECTORY=$INSTALL_DIR "
 CMAKE_FLAGS+="-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR "
-CMAKE_FLAGS+="-DCMAKE_PREFIX_PATH=$LIBINT2_DIR "
+CMAKE_FLAGS+="-DCMAKE_PREFIX_PATH=$LIBINT2_INSTALL "
 CMAKE_FLAGS+="-DBUILD_SHARED_LIBS=ON "
 CMAKE_FLAGS+="-DCMAKE_POSITION_INDEPENDENT_CODE=ON "
 CMAKE_FLAGS+="-DCMAKE_CXX_STANDARD=17 "
 cmake -S . -B build $CMAKE_FLAGS
+
+
